29504b5330c28f8c2b962fd0849e0f77fc5460d7
nhatngk <nhat.ngk@gmail.com>
2024-06-18 18:46:04 +0700

    send text message

--- client/src/api/privateApi.js
+++ client/src/api/privateApi.js
@@ -14,7 +14,7 @@ privateApi.interceptors.response.use(
     async error => {
         const originalRequest = error.config;
 
-        if (error?.response?.data?.statusCode === 401 && !originalRequest._retry) {
+        if (error?.response?.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true;
             return refreshToken()
                 .then(() =>  privateApi(originalRequest))
